64ef62767f2726687984712eb8f02bfe85edc695
Fabian Hinsenkamp <Fabian.Hinsenkamp@camunda.com>
2018-12-02 19:14:00 +0100

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,47 @@
+// http://eslint.org/docs/user-guide/configuring
+
+module.exports = {
+  root: true,
+  parserOptions: {
+    sourceType: 'module',
+    parser: 'babel-eslint'
+  },
+  env: {
+    browser: true,
+    es6: true,
+    jest: true
+  },
+  extends: ['standard', 'prettier', 'plugin:vue/essential'],
+  // required to lint *.vue files
+  plugins: ['html'],
+  // check if imports actually resolve
+  settings: {
+    'import/resolver': {
+      webpack: {
+        config: 'build/webpack.base.conf.js'
+      }
+    }
+  },
+  // add your custom rules here
+  rules: {
+    // don't require .vue extension when importing
+    'import/extensions': [
+      'error',
+      'always',
+      {
+        js: 'never',
+        vue: 'off'
+      }
+    ],
+    'func-names': 0,
+    // allow optionalDependencies
+    'import/no-extraneous-dependencies': [
+      'error',
+      {
+        optionalDependencies: ['test/unit/index.js']
+      }
+    ],
+    // allow debugger during development
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
+  }
+};
---
+++ src/components/Hello.test.js
@@ -0,0 +1,26 @@
+import Vue from 'vue';
+import jsdom from 'jsdom';
+import HelloComponent from './Hello.vue';
+
+const renderer = require('vue-server-renderer').createRenderer();
+
+describe('Test suite for HelloComponent', () => {
+  it('Test data msg', () => {
+    const ClonedComponent = Vue.extend(HelloComponent);
+    const NewComponent = new ClonedComponent({
+      data() {
+        return {
+          msg: 'I am a cool message'
+        };
+      }
+    }).$mount();
+    renderer.renderToString(NewComponent, (err, str) => {
+      if (err) {
+        console.log(err);
+      }
+      const dom = new jsdom.JSDOM(str);
+      const firstHeading = dom.window.document.querySelector('h1');
+      expect(firstHeading.textContent).toContain('I am a cool message');
+    });
+  });
+});
