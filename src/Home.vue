aba36875507acf962c8e83049739eb8b79cf1752
Fabian Hinsenkamp <Fabian.Hinsenkamp@camunda.com>
2018-12-10 21:11:30 +0100

    add result component

--- src/Home.vue
+++ src/Home.vue
@@ -12,8 +12,15 @@
           class="form-body"
           slot="body"
         >
-          <InputForm @submitted="submitted" />
-
+          <InputForm
+            v-if="!resultsCalculated"
+            @submitted="submitted"
+          />
+          <Result
+            v-if="resultsCalculated"
+            :results="calculations"
+            @clearInputData="clearIncomeData"
+          />
         </template>
       </Panel>
     </b-row>
@@ -22,17 +29,24 @@
 <script>
 import Panel from "./components/Panel.vue";
 import InputForm from "./components/InputForm.vue";
+import Result from "./components/Result.vue";
 
 import { calcChurchTax, calcIncomeTax, calcSoli } from "./calc.js";
 
 export default {
   name: "Home",
-  components: { Panel, InputForm },
+  components: { Panel, InputForm, Result },
   data() {
     return {
       calculations: ""
     };
   },
+  computed: {
+    resultsCalculated: function() {
+      return Object.keys(this.calculations).length !== 0;
+    }
+  },
+
   methods: {
     submitted: function(input) {
       const incomeValue = parseInt(input.incomeValue);
@@ -43,19 +57,22 @@ export default {
       );
       const soli = calcSoli(incomeTax, input.relationship);
 
-      const churchtax = input.isInChurch
+      const churchTax = input.isInChurch
         ? calcChurchTax(incomeTax, input.stateOfResidence)
         : 0;
       const netIncome =
-        ((incomeValue - incomeTax - soli - churchtax) * 100) / 100;
+        ((incomeValue - incomeTax - soli - churchTax) * 100) / 100;
 
       this.calculations = {
         incomeValue,
         incomeTax,
         soli,
-        churchtax,
+        churchTax,
         netIncome
       };
+    },
+    clearIncomeData: function() {
+      this.calculations = "";
     }
   }
 };
---
+++ src/components/Result.vue
@@ -0,0 +1,113 @@
+<template>
+  <div>
+    <table>
+      <tr class="table-head">
+        <th class="name">Name</th>
+        <th class="value">Value</th>
+      </tr>
+      <tr class="calc-base">
+        <td>Gross Income</td>
+        <td class="value">
+          {{ results.incomeValue }}
+        </td>
+      </tr>
+      <tr>
+        <td>Income Tax</td>
+        <td class="value">
+          - {{ results.incomeTax }}
+        </td>
+      </tr>
+      <tr>
+        <td>Church Tax</td>
+        <td class="value">
+          - {{ results.churchTax }}
+        </td>
+      </tr>
+      <tr>
+        <td>Solidarity Charge</td>
+        <td class="value">
+          - {{ results.soli }}
+        </td>
+      </tr>
+      <tr class="calc-result">
+        <td>Net Income</td>
+        <td class="value">
+          {{ results.netIncome }}
+        </td>
+      </tr>
+    </table>
+
+    <b-button
+      v-on:click="handleBackClick"
+      class="btn-back"
+      variant="outline-secondary"
+      size="sm"
+    >
+      Back
+    </b-button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    results: Object
+  },
+  methods: {
+    handleBackClick: function() {
+      this.$emit("clearInputData", {});
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+table {
+  width: 100%;
+}
+
+td {
+  padding: 5px 0;
+}
+
+.euro_icon {
+  fill: rgba(0, 0, 0, 0.8);
+  width: 20px;
+  height: 20px;
+}
+
+.table-panel {
+  display: flex;
+  flex-grow: 1;
+  flex-direction: column;
+  align-items: center;
+  box-shadow: 3px 3px 3px 3px rgba(0, 0, 0, 0.12);
+  border-radius: 2px;
+}
+.btn-back {
+  margin-top: 10px;
+  border-color: $darkAccent;
+  color: $darkAccent;
+
+  &:hover {
+    background: $darkAccent;
+    color: white;
+  }
+}
+
+.table-head {
+  color: $darkShades;
+}
+
+.value {
+  text-align: right;
+}
+
+.calc-row {
+  padding: 0 55px;
+}
+.calc-result {
+  position: relative;
+  border-top: 3px solid #f4f5f5;
+}
+</style>
